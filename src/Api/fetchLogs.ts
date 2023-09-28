8b51fdca4e3c8bd59e5337b0f8654299eb416cf1
Max <santiago.farfan2001@gmail.com>
2023-09-28 11:30:54 -0400

    update

--- src/Api/fetchLogs.ts
+++ src/Api/fetchLogs.ts
@@ -3,7 +3,7 @@ import { APILogResponse } from "./APIResponseTypes";
 import axios from "axios";
 
 const fetchLogs: QueryFunction<APILogResponse> = async () => {
-  const res = await axios.get(`http://maxfarfan.me:3000/logs`);
+  const res = await axios.get(`https://maxfarfan.me/api`);
   return res.data;
 };
 
